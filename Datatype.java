40cc3e00dfe274d56903d812bb8d163bc50ee34d
Niteshhhgupta <113141281+Niteshhhgupta@users.noreply.github.com>
2022-09-13 10:40:09 +0530

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+class Datatypes
+{
+	public static void main(String[] args)
+		{
+			byte a=2;
+			short b=4;
+			int c=10;
+			long d=1000;
+			double e=2000;
+			float f=2556;
+			char g='a';
+			String h="Monu";
+			Boolean j=true;
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+			System.out.println(f);
+			System.out.println(g);
+			System.out.println(h);
+			System.out.println(j);
+		}
+}
